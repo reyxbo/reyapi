f7d962274b5484b1e7f85d82939265263f43e28f
ReyXBo <reyxbo@163.com>
2024-05-12 12:31:13 +0800

    Update.

--- rbaidu/rbaidu_base.py
+++ rbaidu/rbaidu_base.py
@@ -9,7 +9,7 @@
 """
 
 
-from typing import Any, List, Dict, Literal
+from typing import Any, List, Dict, Literal, Optional
 from datetime import datetime
 from requests import Response
 from uuid import uuid1
@@ -31,7 +31,8 @@ class RAPIBaidu(object):
     def __init__(
         self,
         key: str,
-        secret: str
+        secret: str,
+        token_valid_seconds: float = 43200
     ) -> None:
         """
         Build `Baidu API` instance.
@@ -40,46 +41,16 @@ class RAPIBaidu(object):
         ----------
         key : API key.
         secret : API secret.
+        token_valid_seconds : Authorization token vaild seconds.
         """
 
         # Set attribute.
         self.key = key
         self.secret = secret
-        self.token = self.get_token()
+        self.token_valid_seconds = token_valid_seconds
         self.cuid = uuid1()
         self.call_records: List[Dict[Literal["time", "data"], Any]] = []
         self.start_time = now()
-
-
-    def request(
-        self,
-        *args: Any,
-        **kwargs: Any
-    ) -> Response:
-        """
-        Request.
-
-        Parameters
-        ----------
-        args : Position arguments of function.
-        kwargs : Keyword arguments of function.
-
-        Returns
-        -------
-        `Response` instance.
-        """
-
-        # Request.
-        response = reytool_request(*args, **kwargs)
-
-        # Check.
-        content_type = response.headers["Content-Type"]
-        if content_type.startswith("application/json"):
-            response_json: Dict = response.json()
-            if "error_code" in response_json:
-                raise AssertionError("Baidu API request failed", response_json)
-
-        return response
 
 
     def get_token(self) -> str:
@@ -111,6 +82,58 @@ class RAPIBaidu(object):
         token = response_json["access_token"]
 
         return token
+
+
+    @property
+    def token(self) -> str:
+        """
+        Get authorization token.
+        """
+
+        # Get parameter.
+        if hasattr(self, "token_time"):
+            token_time: datetime = getattr(self, "token_time")
+        else:
+            token_time = None
+        if (
+            token_time is None
+            or (now() - token_time).seconds > self.token_valid_seconds
+        ):
+            self.token_time = now()
+            self._token = self.get_token()
+
+        return self._token
+
+
+    def request(
+        self,
+        *args: Any,
+        **kwargs: Any
+    ) -> Response:
+        """
+        Request.
+
+        Parameters
+        ----------
+        args : Position arguments of function.
+        kwargs : Keyword arguments of function.
+
+        Returns
+        -------
+        `Response` instance.
+        """
+
+        # Request.
+        response = reytool_request(*args, **kwargs)
+
+        # Check.
+        content_type = response.headers["Content-Type"]
+        if content_type.startswith("application/json"):
+            response_json: Dict = response.json()
+            if "error_code" in response_json:
+                raise AssertionError("Baidu API request failed", response_json)
+
+        return response
 
 
     def record_call(
